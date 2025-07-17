6298563d3f5a890c5a4a0346f9b2ce160bb808b4
YAHW-R <157525442+YAHW-R@users.noreply.github.com>
2025-07-16 21:11:49 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.API_RANDOM_BIBLE ?? 'https://api-random-bible.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
   plugins: [react(), tailwindcss()],
 })
